4858d8fe9c0b34dc39eb5b4a5e070da0ba029415
Saron Hailemichael <118555824+Michaelsaron@users.noreply.github.com>
2023-01-26 16:09:19 +0300

    Add files via upload

---
+++ metadata.java
@@ -0,0 +1,48 @@
+package metadata;
+
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+
+public class metadata <T,K>
+{
+ T x;
+ int y;
+ K key;
+ MetaData(T t,K k){
+   this.key y=k;
+   this.k=t
+ }
+ void displayAnyarray(y[]anyarr)){
+    for(y x: anyarr )
+    system.out.println(k);
+ } }
+
+    public static void main (String[] args){
+    
+    //MetaData<String,Integer> md1= new MetaData<String,Integer>{"abc",178}
+   // MetaData<Double,Integer> md2= new MetaData<Double,Integer>(12.0,127);
+      
+  LinkedList<string>names=new LinkedList();
+      names.add("barbra");
+      names.add("alora");
+      names.add("teo");
+      names.add("steve");
+      
+    system.out.println(names.s);
+    Iterator it=names.Iterator();
+    }
+while(it.hasNext()){
+  
+  system.out.println(it.next());
+}
+
+ Map<strings.Integer> countrycode = new HashMap<string, Integer>();
+  countrycode.put("ETB", 251);
+  countrycode.put("usa", 342);
+  countrycode.put("uk", 134); 
+    system.out.println(countrycode.keyset());
+      syst.out.println(countrycode.values());
+    
+  }
